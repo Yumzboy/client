11e2995548a40b91c45944bd70ef448a4459d785
Jhon Esparragoza <whysoboredyum@gmail.com>
2023-01-17 21:06:00 -0400

    Rutas añadidas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import TaskForm from './components/TaskForm';
+import TaskList from "./components/TaskList";
+
 export default function App() {
   return (
     <div>
-      Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<TaskList/>} />
+          <Route path='/tasks/new' element={<TaskForm/>} />
+          {/*Edit Route*/}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
